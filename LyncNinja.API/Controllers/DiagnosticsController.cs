799391c6de93629ec12b8a70dc1ddaf54c0896a0
Luke Cusolito <Luke.Cusolito@Outlook.com>
2019-07-27 14:33:26 +1000

    Add diagnostics endpoint. Add production config

---
+++ LyncNinja.API/Controllers/DiagnosticsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace LyncNinja.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiagnosticsController : Controller
+    {
+        /// <summary>
+        /// Verifies system status and other diagnostics information
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Status")]
+        public IActionResult Status()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string version = FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
+
+            return Ok(new
+            {
+                ApplicationName = assembly.GetName().Name,
+                ApplicationHost = Environment.MachineName,
+                Version = version
+            });
+        }
+    }
+}
